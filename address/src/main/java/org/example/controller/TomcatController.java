50993ffab83734a2b065dc3e5ad84a93b1d92f9a
vijayachanta <vijaydev8143@gmail.com>
2023-06-18 01:08:19 +0530

    new app

---
+++ address/src/main/java/org/example/controller/TomcatController.java
@@ -0,0 +1,21 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@RestController
+public class TomcatController {
+
+    @GetMapping("/hello")
+    public Collection<String> sayHello() {
+        return IntStream.range(0, 10)
+                .mapToObj(i -> "Hello number " + i)
+                .collect(Collectors.toList());
+    }
+}
